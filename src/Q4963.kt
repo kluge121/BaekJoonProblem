69b8c08a29e10e89b661c67d29d9c58348c97a78
kluge121 <kluge0221@gmail.com>
2018-10-19 13:41:15 +0900

    18/10/19 Q1012 - solve

--- src/Q4963.kt
+++ src/Q4963.kt
@@ -3,16 +3,14 @@ import kotlin.collections.ArrayList
 
 object Q4963 {
 
-
-    val IMPOSSIBLE = -1
-    val POSSIBLE = 1
+    val VISIT = 1
     val NONE = 0;
+    val ISLAND = 1;
 
-
-    var cache = Array(50) { IntArray(50) }
-    var mX = 0;
-    var mY = 0;
-
+    var cache = Array(51) { IntArray(51) }
+    var mX = 0;//너비
+    var mY = 0;//높이
+    lateinit var mMap: Array<IntArray>
 
     @JvmStatic
     fun main(args: Array<String>) {
@@ -23,72 +21,77 @@ object Q4963 {
             val tmpCase = Case()
             tmpCase.x = sc.nextInt()
             tmpCase.y = sc.nextInt()
-            tmpCase.map = Array(tmpCase.x) { IntArray(tmpCase.y) }
+            tmpCase.map = Array(tmpCase.x + 1) { IntArray(tmpCase.y + 1) }
             if (tmpCase.x == 0 && tmpCase.y == 0)
                 break
-            for (i in 0 until tmpCase.y) {
-                for (j in 0 until tmpCase.x) {
-                    tmpCase.map!![i][j] = sc.nextInt()
+
+            //지도 입력순서는 x축 먼저 입력되고 y축 이동
+            for (i in 1..tmpCase.y) {
+                for (j in 1..tmpCase.x) {
+                    tmpCase.map!![j][i] = sc.nextInt()
                 }
             }
+            caseList.add(tmpCase)
         }
 
+        //테스트 케이스 하나씩 가져와서 실행
         for (case in caseList) {
+
+            //방문기록 초기화
+            mMapInit()
+            var count = 0;
+
+            //전역변수 초기화
             mX = case.x
             mY = case.y
+            mMap = case.map!!
+
+            // 육지만 탐색 시작
+            for (i in 1..mX) {
+                for (j in 1..mY) {
+                    if (cache[i][j] == NONE && case.map!![i][j] == ISLAND) {
+                        search(i, j)
+                        count++
+                    }
+                }
+            }
+            System.out.println(count)
         }
 
-
     }
-
 
     fun search(x: Int, y: Int) {
 
-        // 움직일 수 있는 8가지 방향!
-        // x+1 y
-        // x-1 y
+        if (cache[x][y] == VISIT) return
 
-        // x y+1
-        // x y-1
-
-        // x+1 y+1
-        // x+1 y-1
-
-        // x-1 y+1
-        // x-1 y-1
-
-        if (cache[x][y] != NONE)
-            return
-        //시작은 각 경로를 탐색하자
-
-        if (isMove(x + 1, y)) {
-            cache[x + 1][y] = 1
-            search(x + 1, y)
-        } else {
-            cache[x + 1][y] = 0;
+        //8방향 탐색
+        for (i in -1..1) {
+            for (j in -1..1) {
+                if (i == 0 && j == 0) continue
+                if (isMove(x + i, y + j) && mMap[x + i][y + j] == ISLAND) {
+                    cache[x][y] = VISIT
+                    search(x + i, y + j)
+                }
+            }
         }
-        if (isMove(x - 1, y)) search(x - 1, y)
-        if (isMove(x, y + 1)) search(x, y + 1)
-        if (isMove(x, y - 1)) search(x, y - 1)
-        if (isMove(x + 1, y + 1)) search(x + 1, y + 1)
-        if (isMove(x + 1, y - 1)) search(x + 1, y - 1)
-        if (isMove(x - 1, y + 1)) search(x - 1, y + 1)
-        if (isMove(x - 1, y - 1)) search(x - 1, y - 1)
-
-
+        return
     }
 
-
-    fun isMove(x: Int, y: Int): Boolean {
-        return (x <= mX && y <= mY)
+    private fun isMove(x: Int, y: Int): Boolean {
+        return (x >= 0 && y >= 0 && x <= mX && y <= mY)
     }
 
+    fun mMapInit() {
+        for (i in 0..50) {
+            Arrays.fill(cache[i], NONE)
+        }
+
+    }
 }
-
 
 data class Case(
         var map: Array<IntArray>? = null,
         var x: Int = 0,
         var y: Int = 0
 
-) {}
+)
--- src/Test.kt
+++ src/Test.kt
@@ -3,5 +3,12 @@ object Test {
     @JvmStatic
     fun main(args: Array<String>) {
 
+
+        for (i in 1..50) {
+            for (j in 1..50) {
+             System.out.print("1 ")
+            }
+            System.out.println()
+        }
     }
 }
