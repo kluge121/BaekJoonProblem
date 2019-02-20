450cd6fb66cc6414f965776702a7fcc07023840c
kluge121 <kluge0221@gmail.com>
2019-02-20 15:31:30 +0900

    19/02/20 Q1890

--- src/baekjoon/Q1890.java
+++ src/baekjoon/Q1890.java
@@ -3,19 +3,24 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Q1890 {
 
     static int N;
     static int[][] map;
-    static long[][] countArray;
-    static long count = 0;
+    static long countArray[][];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         N = Integer.parseInt(br.readLine());
         map = new int[N][N];
         countArray = new long[N][N];
+
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(countArray[i], -1);
+        }
 
         for (int i = 0; i < N; i++) {
             String[] a = br.readLine().split(" ");
@@ -23,25 +28,39 @@ public class Q1890 {
                 map[i][j] = Integer.parseInt(a[j]);
             }
         }
-        dfs(0, 0, 1);
-        System.out.println(countArray[N - 1][N - 1]);
+        System.out.println(dfs(0, 0));
     }
 
-    static void dfs(int x, int y, long preSum) {
+    static long dfs(int x, int y) {
 
-        System.out.println(x +" " + y);
-        countArray[x][y] += preSum;
-        if (map[x][y] == 0) {
-            return;
+        if (x == N - 1 && y == N - 1) {
+            return 1;
         }
+
+        if (countArray[x][y] != -1)
+            return countArray[x][y];
+
         int move = map[x][y];
-        if (isValidPoint(x, y + move)) {
-            dfs(x, y + move, countArray[x][y]);
 
-        }
-        if (isValidPoint(x + move, y)) {
-            dfs(x + move, y, countArray[x][y]);
-        }
+        if (move == 0) return 0;
+
+        countArray[x][y] = 0;
+
+        if (isValidPoint(x, y + move))
+            countArray[x][y] += dfs(x, y + move);
+
+        if (isValidPoint(x + move, y))
+            countArray[x][y] += dfs(x + move, y);
+
+
+
+        return countArray[x][y];
+
+
+
+
+
+
     }
 
     static boolean isValidPoint(int x, int y) {
