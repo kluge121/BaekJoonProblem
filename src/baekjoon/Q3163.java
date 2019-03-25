ab746a235ff3f4a17524c10ec00f302d213bff30
kluge121 <kluge0221@gmail.com>
2019-03-25 18:20:40 +0900

    19/03/19 - 3 mac

--- src/baekjoon/Q3163.java
+++ src/baekjoon/Q3163.java
@@ -1,113 +1,97 @@
 
 package baekjoon;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.*;
 
 public class Q3163 {
 
-    public static void main(String[] args) throws IOException {
+    static PriorityQueue<Point> queue;
+    static ArrayList<Point> list;
+    static Point[] outList;
 
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
 
+        Deque d = new LinkedList();
+
+
+        // - 개미 왼쪽
+        // + 개미 오른쪽
+        // 을 바라보고 있다요.
+
         for (int t = 0; t < T; t++) {
+
             String[] a = br.readLine().split(" ");
+
             int N = Integer.parseInt(a[0]);
             int L = Integer.parseInt(a[1]);
-            int K = Integer.parseInt(a[2]);
-
-            Point[] sortList = new Point[N];
-            Point[] orderList = new Point[N];
-            ResultO[] resultList = new ResultO[N];
+            int k = Integer.parseInt(a[2]);
+            int outN = 0;
+            queue = new PriorityQueue<>();
+            list = new ArrayList<>();
+            outList = new Point[N];
 
             for (int i = 0; i < N; i++) {
                 String[] b = br.readLine().split(" ");
                 int position = Integer.parseInt(b[0]);
                 int id = Integer.parseInt(b[1]);
-                int time;
-                if (id > 0) {
-                    time = L - position;
-                } else {
-                    time = position;
-                }
-                Point p = new Point(id, time);
-                sortList[i] = p;
-                orderList[i] = p;
-            }
-            Arrays.sort(sortList);
 
-            int start = 0;
-            int end = N - 1;
-            int preValue = 999999;
+                if (id > 0)
+                    queue.add(new Point(id, L - position));
+                else if (id < 0)
+                    queue.add(new Point(id, position));
+
+                list.add(new Point(id, position));
+            }
+
+
             for (int i = 0; i < N; i++) {
-                int index = i;
-                if (preValue == sortList[i].outTime) {
-                    index = i - 1;
+                Point p = queue.poll();
+                if (p.index > 0) {
+                    Point tmp = list.remove(list.size() - 1);
+                    outList[outN++] = (new Point(tmp.index, p.time));
+                } else if (p.index < 0) {
+                    Point tmp = list.remove(0);
+                    outList[outN++] = (new Point(tmp.index, p.time));
                 }
-                if (sortList[i].id > 0) {
-                    resultList[i] = new ResultO(index, orderList[end--].id, sortList[i].outTime);
-                } else {
-                    resultList[i] = new ResultO(index, orderList[start++].id, sortList[i].outTime);
-                }
-                preValue = sortList[i].outTime;
             }
-            Arrays.sort(resultList);
-            System.out.println(resultList[K - 1].id);
 
+
+
+            for(Point p : outList){
+                System.out.println(p.toString());
+            }
+            System.out.println(outList[k - 1].index);
         }
 
     }
+
 
     static class Point implements Comparable<Point> {
-        int id;
-        int outTime;
 
-        public Point(int id, int outTime) {
-            this.id = id;
-            this.outTime = outTime;
-        }
+        int index, time;
 
-        @Override
-        public int compareTo(Point o) {
-            if (outTime > o.outTime) {
-                return 1;
-            } else if (outTime < o.outTime) {
-                return -1;
-            } else {
-                if (id > o.id) {
-                    return 1;
-                } else if (id < o.id) {
-                    return -1;
-                }
-            }
-            return 0;
-        }
-    }
-
-    static class ResultO implements Comparable<ResultO> {
-        int index;
-        int id;
-        int time;
-
-        public ResultO(int index, int id, int time) {
+        public Point(int index, int time) {
             this.index = index;
-            this.id = id;
             this.time = time;
         }
 
         @Override
-        public int compareTo(ResultO o) {
-            if (index > o.index) {
+        public int compareTo(Point o) {
+            if (time > o.time) {
                 return 1;
-            } else if (index < o.index) {
+            } else if (time < o.time) {
                 return -1;
             } else {
-                if (id > o.id) {
+                if (index > o.index) {
                     return 1;
-                } else if (id < o.id) {
+                } else if (index < o.index) {
                     return -1;
                 }
             }
@@ -116,13 +100,11 @@ public class Q3163 {
 
         @Override
         public String toString() {
-            return "ResultO{" +
+            return "Point{" +
                     "index=" + index +
-                    ", id=" + id +
                     ", time=" + time +
                     '}';
         }
     }
-
 
 }
