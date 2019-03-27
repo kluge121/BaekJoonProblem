1dc22925744838fa173c32ac0c8984f88717fd73
kluge121 <alstn225@gmail.com>
2019-03-27 17:59:31 +0900

    19/03/27 - 1

--- src/baekjoon/Q16000.java
+++ src/baekjoon/Q16000.java
@@ -24,6 +24,7 @@ public class Q16000 {
     static ArrayList<Integer> cycleList;
     static ArrayList<Point> nonCycleList;
     static boolean[] isCycle;
+    static boolean[] isFinish;
 
     static int[] rowD = {-1, 0, 1, 0};
     static int[] colD = {0, 1, 0, -1};
@@ -31,6 +32,7 @@ public class Q16000 {
     static int start;
     static int end;
 
+    //반복문안에서 해당 섬에 사이클 구간이 있는지 체크
     static boolean isFlag;
 
     public static void main(String[] args) throws IOException {
@@ -45,13 +47,13 @@ public class Q16000 {
         nonCycleList = new ArrayList<>();
         isl_seq = 0;
         visit = new boolean[row][column];
+        isFinish = new boolean[column * row];
         isCycle = new boolean[column * row];
-
+        discover = new int[row * column];
         for (int i = 0; i < row; i++) {
             char[] b = br.readLine().toCharArray();
             for (int j = 0; j < column; j++) map[i][j] = b[j];
         }
-
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < column; j++) {
                 if (map[i][j] == '#' && !visit[i][j] && !isSige(i, j)) {
@@ -59,7 +61,7 @@ public class Q16000 {
                     end = -1;
                     isl_seq++;
                     isFlag = false;
-                    discover = new int[row * column];
+
                     dis_seq = 1;
                     makeSpanningTree(i, j, -1);
                     if (isFlag) {
@@ -70,21 +72,35 @@ public class Q16000 {
                 }
             }
         }
-
-//        System.out.println(cycleList.size() + "개");
-//        System.out.println(nonCycleList.size() + "개");
-
-
-//        테스트 출력용
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                if (map[i][j] == '#' && imap[i][j] == 0) {
+                    sb.append("X");
+                } else if (map[i][j] == '#' && imap[i][j] > 0) {
+                    sb.append("O");
+                } else {
+                    sb.append(".");
+                }
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
+//
+//        System.out.println("사이클 " + cycleList.size() + "개");
+//        System.out.println("논사이클 " + nonCycleList.size() + "개");
+//
 //        for (int i = 0; i < row; i++) {
 //            for (int j = 0; j < column; j++) {
-//                System.out.print(imap[i][j] + " ");
+//                if (isCycle[i * column + j]) {
+//                    System.out.print("# ");
+//                } else {
+//                    System.out.print("- ");
+//                }
 //            }
 //            System.out.println();
 //        }
-//        System.out.println(Arrays.toString(isCycle));
     }
-
 
     static boolean makeSpanningTree(int r, int c, int prev) {
         int nowNum = r * column + c;
@@ -95,16 +111,18 @@ public class Q16000 {
         }
         boolean flag = false;
         for (int i = 0; i < 4; i++) {
+
             int cr = r + rowD[i];
             int cc = c + colD[i];
             int next = cr * column + cc;
+
             if (isValid(cr, cc) && map[cr][cc] == '#') {
-                if (discover[next] == 0) {
-                    if (makeSpanningTree(cr, cc, nowNum) && discover[nowNum] > discover[start] && discover[nowNum] < discover[end]) {
+                if (!visit[cr][cc] && discover[next] == 0) {
+                    if (makeSpanningTree(cr, cc, nowNum)) {
                         isCycle[nowNum] = true;
                         flag = true;
                     }
-                } else if (nowNum != prev && discover[nowNum] > discover[next]) {
+                } else if (!isFinish[next] && prev != next && discover[nowNum] > discover[next]) {
                     isCycle[nowNum] = true;
                     isCycle[next] = true;
                     start = next;
@@ -114,12 +132,14 @@ public class Q16000 {
                 }
             }
         }
+        isFinish[nowNum] = true;
         return flag;
     }
 
     static boolean isValid(int r, int c) {
         return r >= 0 && c >= 0 && r < row && c < column;
     }
+
     static class Point {
         int r, c;
 
@@ -128,6 +148,7 @@ public class Q16000 {
             this.c = c;
         }
     }
+
     static boolean isSige(int r, int c) {
         int aa = -1;
         int bb = -1;
