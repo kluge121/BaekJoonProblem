80bdf38da792d9fdf84036c13bef770705dbee5e
kluge121 <alstn225@gmail.com>
2019-04-02 09:01:29 +0900

    19/04/02 - mac merge

--- src/Test.java
+++ src/Test.java
@@ -5,10 +5,16 @@ public class Test {
 
     public static void main(String[] args) {
 
-        for(int i = 1 ; i<=2999; i++){
-            System.out.println(i+" "+(i+1));
-        }
-        System.out.println("1 3000");
+
+        String a = "abcdef";
+
+        String b = "";
+
+
+        System.out.println(a.substring(0,b.length()));
+
+
+
     }
 
 
---
+++ src/baekjoon/Q12100.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q12100 {
+
+    static int N;
+    static int[][] map;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        map = new int[][]
+
+
+    }
+}
